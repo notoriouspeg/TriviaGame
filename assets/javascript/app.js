1208083a6bd1b2fd82e7ff0bf92756e25f3d46a8
notoriousp <pegmickelson+school@gmail.com>
2018-06-21 23:22:48 -0500

    Fixed js file name. Corrected spelling of Cocteau Twins. Commented out Start button.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,78 @@
+(function() {
 
 
-(function(){
+//     var time = 5;
+// var timer;
 
-var time = 5;
-var timer;
+// function timer() {
+//     if (time >= 1) {
+//        time--
+//        console.log(time);
+//    $("#time").html(time);
+//    }
+//    }
 
-function timer() {
-    if (time >= 1) {
-       time--
-       console.log(time);
-   $("#time").html(time);
-   }
-   }
-
-   $("#start").on("click", function(){
-    console.log("click");
-    timer = setInterval(timer, 1000);
-    $("#quiz").html(myQuestions[0].question)
-    for (var i=0; i<4; i++){
+//    $("#start").on("click", function(){
+//     console.log("click");
+//     timer = setInterval(timer, 1000);
+//     $("#quiz").html(myQuestions[0].question)
+//     for (var i=0; i<4; i++){
     
-    var isChecked = $('#rdSelect:checked').val()?true:false;
-    }
-    })
+//     var isChecked = $('#rdSelect:checked').val()?true:false;
+//     }
+//     })
+    
+  function buildQuiz() {
 
-const quizContainer = document.getElementById('quiz');
-const resultsContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-const myQuestions = [
+    const output = [];
+
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+        const answers = [];
+
+      for (letter in currentQuestion.answers) {
+        answers.push(
+          `<label>
+            <input type="radio" name="question${questionNumber}" value="${letter}">
+            ${letter} :
+            ${currentQuestion.answers[letter]}
+          </label>`
+        );
+      }
+
+      output.push(
+        `<div class="question"> ${currentQuestion.question} </div>
+        <div class="answers"> ${answers.join("")} </div>`
+      );
+    });
+
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults() {
+    const answerContainers = quizContainer.querySelectorAll(".answers");
+
+    let numCorrect = 0;
+
+    myQuestions.forEach((currentQuestion, questionNumber) => {
+
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+
+      if (userAnswer === currentQuestion.correctAnswer) {
+        numCorrect++;
+    } else {
+      }
+    });
+
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+  const quizContainer = document.getElementById("quiz");
+  const resultsContainer = document.getElementById("results");
+  const submitButton = document.getElementById("submit");
+  const myQuestions = [
     {
       question: "Which 80's band's members were named Simon, Nick, John, Andy and Roger??",
       answers: {
@@ -40,7 +86,7 @@ const myQuestions = [
       question: "Who sang the hit 'Tainted Love'?",
       answers: {
         a: "Bananarama",
-        b: "Cocteu Twins",
+        b: "Cocteau Twins",
         c: "Soft Cell"
       },
       correctAnswer: "c"
@@ -56,172 +102,7 @@ const myQuestions = [
     }
   ];
 
-function buildQuiz(){
-    const output = [];
-  
-    myQuestions.forEach(
-      (currentQuestion, questionNumber) => {
-  
-        const answers = [];
-  
-        for(letter in currentQuestion.answers){
-  
-          answers.push(
-            `<label>
-              <input type="radio" name="question${questionNumber}" value="${letter}">
-              ${letter} :
-              ${currentQuestion.answers[letter]}
-            </label>`
-          );
-        }
-  
-        output.push(
-          `<div class="question"> ${currentQuestion.question} </div>
-          <div class="answers"> ${answers.join('')} </div>`
-        );
-      }
-    );
-  
-    quizContainer.innerHTML = output.join('');
-  }
+  buildQuiz();
 
-  myQuestions.forEach( (currentQuestion, questionNumber) => {
-    // here goes the code we want to run for each question
-  });
-
-function showResults(){}
-
-// we'll want to store the list of answer choices
-const answers = [];
-
-// and for each available answer...
-for(letter in currentQuestion.answers){
-
-  // ...add an html radio button
-  answers.push(
-    `<label>
-      <input type="radio" name="question${questionNumber}" value="${letter}">
-      ${letter} :
-      ${currentQuestion.answers[letter]}
-    </label>`
-  );
-}
-
-quizContainer.innerHTML = output.join('');
-
-function showResults(){
-
-    // gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll('.answers');
-  
-    // keep track of user's answers
-    let numCorrect = 0;
-  
-    // for each question...
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
-  
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = 'input[name=question'+questionNumber+']:checked';
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-      // if answer is correct
-      if(userAnswer===currentQuestion.correctAnswer){
-        // add to the number of correct answers
-        numCorrect++;
-  
-        // color the answers green
-        answerContainers[questionNumber].style.color = 'lightgreen';
-      }
-      // if answer is wrong or blank
-      else{
-        // color the answers red
-        answerContainers[questionNumber].style.color = 'red';
-      }
-    });
-  
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = numCorrect + ' out of ' + myQuestions.length;
-  }
-
-  // gather answer containers from our quiz
-const answerContainers = quizContainer.querySelectorAll('.answers');
-
-// keep track of user's answers
-let numCorrect = 0;
-
-// for each question...
-myQuestions.forEach( (currentQuestion, questionNumber) => {
-
-    // find selected answer
-    const answerContainer = answerContainers[questionNumber];
-    const selector = `input[name=question${questionNumber}]:checked`;
-    const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-  
-    // if answer is correct
-    if(userAnswer===currentQuestion.correctAnswer){
-      // add to the number of correct answers
-      numCorrect++;
-  
-      // color the answers green
-      answerContainers[questionNumber].style.color = 'lightgreen';
-    }
-    // if answer is wrong or blank
-    else{
-      // color the answers red
-      answerContainers[questionNumber].style.color = 'red';
-    }
-  });
-  // if answer is correct
-if(userAnswer===currentQuestion.correctAnswer){
-    // add to the number of correct answers
-    numCorrect++;
-  
-    // color the answers green
-    answerContainers[questionNumber].style.color = 'lightgreen';
-  }
-  // if answer is wrong or blank
-  else{
-    // color the answers red
-    answerContainers[questionNumber].style.color = 'red';
-  }
-// add this question and its answers to the output
-output.push(
-  `<div class="question"> ${currentQuestion.question} </div>
-  <div class="answers"> ${answers.join('')} </div>`
-);
-
-// display quiz right away
-buildQuiz();
-
-// on submit, show results
-submitButton.addEventListener('click', showResults);
-
-
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-	function showQuestions(questions, quizContainer){
-		var output = [];
-        var answers;
-	}
-
-	function showResults(questions, quizContainer, resultsContainer){
-		// code will go here
-	}
-
-	// show the questions
-	showQuestions(questions, quizContainer);
-
-	// when user clicks submit, show results
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
-	}
-}
-
-// show number of correct answers out of total
-resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
-
+  submitButton.addEventListener("click", showResults);
 })();
-
-//Game reset
-// startGame();
